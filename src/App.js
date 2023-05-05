cc0663e4de12349d36a7d3e120e852a0a41d062a
Peter Akhigbe <akhigbe37@gmail.com>
2023-05-05 16:16:21 +0100

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/categories" element={<Categories/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
