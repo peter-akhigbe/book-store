7dabd24310d9dcffa11eb3e87a196605c33903e5
Peter Akhigbe <akhigbe37@gmail.com>
2023-05-13 12:34:19 +0100

    Use Redux in React components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,28 +1,35 @@
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-export default function List({ books }) {
+const List = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books);
+
+  const handleRemoveBook = (itemId) => {
+    dispatch(removeBook(itemId));
+  };
+
   return (
     <div>
       <h2>Books</h2>
-      <ul>
+      <ul className="bookDisplay">
         {books.map((book) => (
-          <li key={book.id}>
-            <div>{book.title}</div>
-            <div>{book.category}</div>
-            <button type="button">Remove</button>
-          </li>
+          <div key={book.item_id}>
+            <li>{book.item_id}</li>
+            <li>{book.title}</li>
+            <li>{book.author}</li>
+            <button
+              type="button"
+              onClick={() => handleRemoveBook(book.item_id)}
+            >
+              Remove Book
+            </button>
+          </div>
         ))}
       </ul>
     </div>
   );
-}
-
-List.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      category: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
 };
+
+export default List;
